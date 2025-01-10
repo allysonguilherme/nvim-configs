1c35d3e667957f8e66e48c5e5240877d487e3b0b
Allyson Guilherme <allysondev.tech@gmail.com>
2025-01-10 16:23:37 -0300

    feat: add clang task to overseer

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
 require("config.lazy")
 require("mason").setup()
 require('overseer').setup({
-    templates = { "builtin", "user.cpp_build", "user.cpp_run" }
+    templates = {
+        "builtin",
+        "user.cpp_build",
+        "user.cpp_run",
+        "user.cpp_clang"
+    }
 })
 
 vim.opt.clipboard = "unnamedplus"
@@ -15,8 +20,15 @@ o.shiftwidth = 4
 
 vim.wo.number = true
 
-require("mason-lspconfig").setup{
+require("mason-lspconfig").setup {
     ensure_installed = { "omnisharp" }
 }
 
-require'lspconfig'.omnisharp.setup{}
+require 'lspconfig'.omnisharp.setup {}
+
+vim.keymap.set({"n"}, "<leader>k", "<cmd>Format<cr>")
+vim.keymap.set({"n"}, "\\o", "<cmd>OverseerRun<cr>")
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
